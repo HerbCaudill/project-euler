601306e3db06f1e4efa29e7b54186e7fc597d854
Herb Caudill <herb@devresults.com>
2019-05-18 15:05:58 +0200

    util/least-common-multiple.test:

--- src/util/least-common-multiple.test.ts
+++ src/util/least-common-multiple.test.ts
@@ -2,14 +2,18 @@ import { leastCommonMultiple } from '.'
 import { range } from './range'
 
 describe('leastCommonMultiple', () => {
-  const testCase = (n: number[], expected: number) =>
+  const testCase = (n: number[], expected: number | undefined) =>
     test(`${JSON.stringify(n)}: ${expected}`, () =>
-      expect(leastCommonMultiple(...n)).toEqual(expected))
+      expect(leastCommonMultiple(n)).toEqual(expected))
 
+  testCase([], undefined)
+  testCase([2], 2)
   testCase([2, 3], 6)
   testCase([4, 6], 12)
   testCase([8, 12], 24)
   testCase([4, 8, 12], 24)
   testCase([5, 8, 12], 120)
+  testCase([120, 81], 3240)
   testCase(range({ start: 2, stop: 10 }), 2520)
+  testCase([4, 7920, 62710561, 71234569], 35379905493899014000)
 })
