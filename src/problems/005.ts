9e891258d8c08709031288e088def84f2598d74a
Herb Caudill <herb@devresults.com>
2019-05-18 15:08:02 +0200

    problems/005: extract nums to variable

--- src/problems/005.ts
+++ src/problems/005.ts
@@ -8,5 +8,6 @@ import { leastCommonMultiple, range } from '../util'
 // What is the smallest number that is evenly divisible by all of the numbers
 // from 1 to 20?
 
-export const solution005 = () =>
-  leastCommonMultiple(range({ start: 2, stop: 20 }))
+const nums = range({ start: 2, stop: 20 })
+
+export const solution005 = () => leastCommonMultiple(nums)
