cae3793258f53a4691f021e9ad955568ba3791b7
Herb Caudill <herb@devresults.com>
2019-05-18 18:01:17 +0200

    009✔

--- src/problems/009.ts
+++ src/problems/009.ts
@@ -17,20 +17,19 @@ interface Triplet {
   c: number
 }
 
-// We're given
+const product = ({ a, b, c }: Triplet) => a * b * c
 
+// We're given
 // a + b + c = 1000
 // a² + b² = c²
 
 // We can eliminate c:
-
 // c = 1000 - a - b
 // a² + b² = (1000 - a - b)²
 
 const C = (a: number, b: number) => 1000 - a - b
 
 // Solve for b:
-
 // a² + b² = a² + 2ab - 2000a + b² - 2000b + 1000000
 // 0 = 2ab - 2000a - 2000b + 1000000
 // 2000b - 2ab = 1000000 - 2000a
@@ -38,34 +37,29 @@ const C = (a: number, b: number) => 1000 - a - b
 // b = (1000000 - 2000a) / (2000 - 2a)
 
 // Simplifying:
-
 // b = (500000 - 1000a) / (1000 - a)
 //   = 1000(500 - a) / (1000 - a)
 
 const B = (a: number) => (1000 * (500 - a)) / (1000 - a)
 
 // a and b and c are positive integers.
-
-// If a is positive, b is positive.
-// If a is an integer and b is an integer, c is an integer.
-
 // a is less than 1000.
-
-// So: We try a from 1 to 1000, and check that:
-// - b is an integer
-// - c is positive
+// So: We try a from 1 to 1000 ...
 
 const A = range({ start: 1, stop: 999 })
 
-const match = (d: Triplet) => Number.isInteger(d.b) && d.c > 0
+// If a is positive, b is positive.
+// If a is an integer and b is an integer, c is an integer.
+// So we just need to check that:
+// - b is an integer
+// - c is positive
 
-const triplet = (x: number): Triplet => {
-  const a = x
+const match = ({ b, c }: Triplet) => Number.isInteger(b) && c > 0
+
+const makeTriplet = (a: number): Triplet => {
   const b = B(a)
   const c = C(a, b)
   return { a, b, c }
 }
 
-const product = (d: Triplet) => d.a * d.b * d.c
-
-export const solution009 = () => product(A.map(triplet).find(match)!)
+export const solution009 = () => product(A.map(makeTriplet).find(match)!)
