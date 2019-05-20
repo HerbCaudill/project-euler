ad3b7995f7b8d124af4a55e7a02f9e8400f27be3
Herb Caudill <herb@devresults.com>
2019-05-20 19:34:15 +0200

    refactor combinations

--- src/lib/combinations.ts
+++ src/lib/combinations.ts
@@ -1,34 +1,34 @@
 import { flatten } from './flatten'
 
 export const combinations = (s: Set<any>, r: number = -1): Set<any>[] => {
-  s = new Set(s)
-  if (s.size === 0 || r === 0 || r > s.size) return []
-  if (s.size === 1) return [s]
+  // work with a copy in array form
+  const arr = [...s]
 
-  if (r === -1) {
-    return [
-      ...[...s].reduce((result, _, i) => {
-        return new Set(combinations(s, i + 1).concat([...result]))
-      }, new Set()),
-    ]
-  } else {
-    if (r === 1) {
+  // edge cases
+  if (arr.length === 0 || r === 0 || r > arr.length) return []
+  if (arr.length === 1) return [new Set(arr)]
+
+  switch (r) {
+    case -1:
+      // return combinations for all values of r
+      return arr.reduce((result, _, rr) => {
+        return combinations(s, rr + 1).concat(result)
+      }, [])
+    case 1:
       // take 1 just returns an array of single-item sets, e.g.
       // combinations({1,2,3,4}, 1) -> [{1}, {2}, {3}, {4}]
-      return [...s].map(d => new Set([d]))
-    } else {
+      return arr.map(d => new Set([d]))
+    default:
       // take r out of n items
-      const copy = new Set(s)
+      const subset = new Set(arr)
       return flatten(
-        [...s].map(d => {
-          copy.delete(d)
-          let subCombinations = combinations(copy, r - 1).map(sc => {
-            sc.add(d)
-            return sc
-          })
-          return subCombinations
+        // for each item, return all combinations of r-1 from N for the remaining items,
+        // plus that item
+        arr.map(d => {
+          subset.delete(d)
+          const subCombinations = combinations(subset, r - 1)
+          return subCombinations.map(sc => new Set([d, ...sc]))
         })
       )
-    }
   }
 }
