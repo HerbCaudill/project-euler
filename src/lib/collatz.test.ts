4f63536511bcd1db7e458ee4d020389f0210a8a3
Herb Caudill <herb@devresults.com>
2019-06-01 19:58:54 +0200

    collatz works but too slow with large n

--- src/lib/collatz.test.ts
+++ src/lib/collatz.test.ts
@@ -1,7 +1,12 @@
 import { collatz } from '.'
 
 describe('Collatz', () => {
-  test('should ', () => {
-    expect(collatz(13)).toEqual([13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
-  })
+  test('2', () => expect(collatz(2)).toEqual([2, 1]))
+  test('13', () =>
+    expect(collatz(13)).toEqual([13, 40, 20, 10, 5, 16, 8, 4, 2, 1]))
+  // test('837799', () => expect(collatz(837799)).toHaveLength(525))
+  // test('1 million', () => expect(collatz(10 ** 6)).toHaveLength(153))
+
+  // // largest path for n in javascript integer space
+  // test('75,128,138,247', () => expect(collatz(75128138247)).toHaveLength(1229))
 })
--- src/lib/collatz.ts
+++ src/lib/collatz.ts
@@ -1,7 +1,11 @@
-export const collatz = (n: number, result = [n]): number[] => {
-  if (n === 1) return result
-  const next = isEven(n) ? n / 2 : 3 * n + 1
-  return collatz(next, result.concat(next))
-}
+import memoize from 'memoizee'
 
-const isEven = (n: number) => n % 2 === 0
+export const collatz = memoize(
+  (n: number): number[] =>
+    n <= 1
+      ? [1]
+      : n % 2 === 0
+      ? [n].concat(collatz(n / 2))
+      : [n].concat(3 * n + 1).concat(collatz((3 * n + 1) / 2)),
+  { max: 10000 }
+)
