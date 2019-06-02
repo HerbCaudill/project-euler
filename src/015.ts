a6835a87be0f422674d236bb4e736efd1f96241f
Herb Caudill <herb@devresults.com>
2019-06-02 11:35:53 +0200

    015 create

---
+++ src/015.ts
@@ -0,0 +1,9 @@
+
+// Lattice paths
+// =============
+// Starting in the top left corner of a 2 * 2 grid, there are 6 routes
+// (without backtracking) to the bottom right corner.
+// 
+// How many routes are there through a 20 * 20 grid?
+
+export const solution015 = () => -1
