55597aa6b7a11d29c44b369ec8941fa1a767f130
Herb Caudill <herb@devresults.com>
2019-06-02 14:51:08 +0200

    15✔

--- src/015.ts
+++ src/015.ts
@@ -1,9 +1,16 @@
+import { pascalTriangle } from './lib'
 
 // Lattice paths
 // =============
 // Starting in the top left corner of a 2 * 2 grid, there are 6 routes
 // (without backtracking) to the bottom right corner.
-// 
+//
 // How many routes are there through a 20 * 20 grid?
 
-export const solution015 = () => -1
+const routesThroughGrid = (size: number) => pascalTriangle(size * 2, size)
+
+expect(routesThroughGrid(1)).toEqual(2)
+expect(routesThroughGrid(2)).toEqual(6)
+expect(routesThroughGrid(3)).toEqual(20)
+
+export const solution015 = () => routesThroughGrid(20)
