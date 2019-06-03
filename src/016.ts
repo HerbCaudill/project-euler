7f44cbf29d72d3bf71f8264bb20ab0d6dad6c4fd
Herb Caudill <herb@devresults.com>
2019-06-03 22:07:18 +0200

    016:

--- src/016.ts
+++ src/016.ts
@@ -1,20 +1,10 @@
-import { sum } from './lib'
+import { powerDigitSum } from './lib'
 
 // Power digit sum
 // ===============
 // 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 //
 // What is the sum of the digits of the number 2^1000?
-
-const powerDigitSum = (a: number) => {
-  const n = BigInt(2) ** BigInt(a)
-  return sum(
-    n
-      .toString()
-      .split('')
-      .map(d => +d)
-  )
-}
 
 expect(powerDigitSum(4)).toEqual(7)
 expect(powerDigitSum(15)).toEqual(26)
