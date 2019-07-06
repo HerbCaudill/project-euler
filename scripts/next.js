500e9fff7f9dd4704afae0ca37c64aca8214c4b4
Herb Caudill <herb@devresults.com>
2019-07-06 10:28:51 +0200

    fix next script

--- scripts/next.js
+++ scripts/next.js
@@ -4,11 +4,11 @@ const problems = require('../src/problems.json')
 const prettier = require('prettier')
 const prettierConfig = require('../package.json').prettier
 
-const nextFile = (dir, template) => {
+const nextFile = (dir, template, suffix = '') => {
   const nextId =
     +fs
       .readdirSync(dir)
-      .map(f => path.parse(f).name)
+      .map(f => path.parse(f).name.replace(suffix, ''))
       .filter(f => Number.isInteger(+f))
       .pop() + 1 || 1
 
@@ -17,7 +17,7 @@ const nextFile = (dir, template) => {
     ...prettierConfig,
     parser: 'typescript',
   })
-  fs.writeFileSync(`${dir}/${nextFileName}.ts`, fileText)
+  fs.writeFileSync(`${dir}/${nextFileName}${suffix}.ts`, fileText)
 }
 
 const problemTemplate = nextId => {
@@ -41,11 +41,11 @@ const testTemplate = nextId => {
   const nextFileName = `${nextId}`.padStart(3, '0')
   const problem = problems.find(p => p.id === nextId)
   return `
-    import { solution${nextFileName} } from '../../src/${nextFileName}'
-    import { executeTest } from '../../src/execute'
+    import { solution${nextFileName} } from '../src/${nextFileName}'
+    import { executeTest } from '../src/execute'
     executeTest(${nextId}, solution${nextFileName})
     `.trim()
 }
 
 nextFile('./src', problemTemplate)
-nextFile('./tests/src', testTemplate)
+nextFile('./tests', testTemplate, '.test')
