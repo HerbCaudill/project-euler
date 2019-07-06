2ec7c2d4d47f83955a3c66795b5be5cc53d0fc79
Herb Caudill <herb@devresults.com>
2019-07-06 16:33:33 +0200

    lib/sum:

--- src/lib/sum.ts
+++ src/lib/sum.ts
@@ -1,5 +1,5 @@
 export const sum = (arr: number[]) =>
   arr.reduce((prev, current) => prev + current, 0)
 
-export const bigSum = (arr: (number | BigInt)[]) =>
-  arr.reduce((prev, current) => BigInt(prev) + BigInt(current), 0n)
+export const bigSum = (arr: (number | bigint)[]) =>
+  arr.reduce((prev, current) => BigInt(prev) + BigInt(current), 0n) as bigint
