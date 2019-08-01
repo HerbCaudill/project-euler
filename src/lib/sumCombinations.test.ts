a406f5ee7ace20b5e612db73194da0d2430ebd82
Herb Caudill <herb@devresults.com>
2019-08-01 09:40:16 +0200

    TCR

--- src/lib/sumCombinations.test.ts
+++ src/lib/sumCombinations.test.ts
@@ -1,7 +1,9 @@
 ﻿import { sumCombinations } from './sumCombinations'
 describe('sumCombinations', () => {
-  it('should ', () => {
+  it('case 1', () => {
     expect(sumCombinations(5, [5])).toEqual([[5]])
+  })
+  it('case 2 ', () => {
     expect(sumCombinations(5, [1, 5])).toEqual([[1, 1, 1, 1, 1], [5]])
   })
 })
