1df37ddef63b1f54513f6381af690713c2e89d3d
Herb Caudill <herb@devresults.com>
2019-08-01 08:13:43 +0200

    TCR

--- src/lib/sumCombinations.ts
+++ src/lib/sumCombinations.ts
@@ -1,8 +1,11 @@
 ﻿/**
- * 
+ * Returns all combinations of a set of numbers that add up to a given total.
  * @param total The number that combinations will add up to
- * @param coins T
+ * @param numbers The numbers being added up
  */
-export const sumCombinations = (total: number, coins: number[]): number[][] => {
+export const sumCombinations = (
+  total: number,
+  numbers: number[]
+): number[][] => {
   return []
 }
