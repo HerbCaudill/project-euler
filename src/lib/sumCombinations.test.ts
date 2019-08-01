bb4be21b65e8f3ab60893c62aca1aafc83efcaf3
Herb Caudill <herb@devresults.com>
2019-08-01 09:43:49 +0200

    TCR

--- src/lib/sumCombinations.test.ts
+++ src/lib/sumCombinations.test.ts
@@ -1,5 +1,6 @@
 ﻿import { sumCombinations } from './sumCombinations'
 describe('sumCombinations', () => {
+  it('$.04, nickels', () => expect(sumCombinations(4, [5])).toEqual([]))
   it('$.05, nickels', () => {
     expect(sumCombinations(5, [5])).toEqual([[5]])
   })
