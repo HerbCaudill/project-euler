64019a1c1bf5f2b517a49f70c9b1922d33512d80
Herb Caudill <herb@devresults.com>
2019-08-01 09:37:04 +0200

    TCR

--- src/lib/sumCombinations.ts
+++ src/lib/sumCombinations.ts
@@ -2,7 +2,7 @@
  * Returns all possible combinations of a set of coin values that add up to a given total.
  *
  * @param total The number that combinations will add up to
- * @param coins The numbers being added up
+ * @param coins The available coin values
  */
 export const sumCombinations = (total: number, coins: number[]): number[][] => {
   return coins.reduce(
