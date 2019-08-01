fcd5e50667f127259d1dcb31182cd935848c7a7a
Herb Caudill <herb@devresults.com>
2019-08-01 08:12:45 +0200

    TCR

--- src/lib/sumCombinations.ts
+++ src/lib/sumCombinations.ts
@@ -1,8 +1,8 @@
 ﻿/**
  * 
- * @param N 
- * @param arr 
+ * @param num The number that combinations will add up to
+ * @param arr T
  */
-export const sumCombinations = (N: number, arr: number[]): number[][] => {
+export const sumCombinations = (num: number, arr: number[]): number[][] => {
   return []
 }
