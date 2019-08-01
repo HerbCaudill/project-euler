248fa8563695b893e7310e8866c4d3cdd62b30f6
Herb Caudill <herb@devresults.com>
2019-08-01 08:05:49 +0200

    TCR

--- src/031.ts
+++ src/031.ts
@@ -11,8 +11,6 @@
 //
 // How many different ways can -L-2 be made using any number of coins?
 
-const coins = [
-  1
-]
+const coins = [1, 2, 5, 10, 20, 50, 100, 200]
 
 export const solution031 = () => -1
