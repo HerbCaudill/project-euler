1fba68e6ae4aaca931e2c44581afeeca44e477ae
Herb Caudill <herb@devresults.com>
2019-08-01 09:36:34 +0200

    TCR

--- src/lib/sumCombinations.ts
+++ src/lib/sumCombinations.ts
@@ -1,13 +1,11 @@
 ﻿/**
- * Returns all combinations of a set of numbers that add up to a given total.
+ * Returns all possible combinations of a set of coin values that add up to a given total.
+ *
  * @param total The number that combinations will add up to
- * @param numbers The numbers being added up
+ * @param coins The numbers being added up
  */
-export const sumCombinations = (
-  total: number,
-  numbers: number[]
-): number[][] => {
-  return numbers.reduce(
+export const sumCombinations = (total: number, coins: number[]): number[][] => {
+  return coins.reduce(
     (previous, current, i) => {
       if (total % current === 0) {
         const count = total / current
