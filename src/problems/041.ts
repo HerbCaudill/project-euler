229bfbb87070eb73d4d3383938a237ad55263de5
Herb Caudill <herb@devresults.com>
2019-09-24 23:52:55 +0200

    problems/041.ts:

--- src/problems/041.ts
+++ src/problems/041.ts
@@ -1,6 +1,5 @@
-import { permutations } from 'lib/permutations'
-import { isPrime } from 'lib/primes'
 import { panDigitals } from 'lib/panDigital'
+import { isPrime } from 'lib/primes'
 
 // Pandigital prime
 // ================
@@ -10,8 +9,17 @@ import { panDigitals } from 'lib/panDigital'
 //
 // What is the largest n-digit pandigital prime that exists?
 
-export const solution041 = () =>
-  [9, 8, 7, 6]
-    .map(panDigitals)
-    .flat()
-    .find(isPrime)
+export const solution041 = () => {
+  // try all 9-digit pandigitals, then all 8-digit, etc.
+  let K = 9
+  while (K > 0) {
+    // get the pandigitals and order by size, with biggest first
+    const candidates = panDigitals(K).sort((a, b) => b - a)
+    // see if we have any primes
+    const success = candidates.find(isPrime)
+    if (success) return success
+    // if not, try pandigitals with one less digit
+    K -= 1
+  }
+  return undefined
+}
