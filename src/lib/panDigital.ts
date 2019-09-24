bd1380f69c409e71d4c43902419f4a48af7620f6
Herb Caudill <herb@devresults.com>
2019-09-24 23:53:02 +0200

    lib/panDigital.ts:

--- src/lib/panDigital.ts
+++ src/lib/panDigital.ts
@@ -1,6 +1,6 @@
 ﻿import { permutations } from './permutations'
 
-const allDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+export const allDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 export const isPandigital = (n: number | string, K: number = 9) =>
   // the number has to have K digits (e.g. 9 digits for 1-to-9 pandigital)
@@ -12,7 +12,4 @@ export const isPandigital = (n: number | string, K: number = 9) =>
       .reduce((result, d) => result.replace(d.toString(), ''), n.toString())
 
 export const panDigitals = (K = 9) =>
-  permutations(allDigits.slice(0, K))
-    .map(arr => +arr.join(''))
-    .sort()
-    .reverse()
+  permutations(allDigits.slice(0, K)).map(arr => +arr.join(''))
