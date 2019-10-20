f84295c3e5630c7d7a978d324ba651eca185bd2f
Herb Caudill <herb@devresults.com>
2019-10-20 11:54:49 +0200

    put 034 comment back

--- src/problems/034.ts
+++ src/problems/034.ts
@@ -3,6 +3,15 @@ import { range } from 'lib/range'
 import { sum } from 'lib/sum'
 import { digits } from '../lib/digits'
 
+// Digit factorials
+// ================
+// 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+//
+// Find the sum of all numbers which are equal to the sum of the factorial of
+// their digits.
+//
+// Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
 const factorials = range({ start: 0, stop: 9 }).map(d => Number(factorial(d)))
 const lookupFactorial = (n: number) => factorials[n]
 
