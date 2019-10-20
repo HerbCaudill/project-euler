eb03eeeac63e6c3f2ef6b6b0971165da807df0bc
Herb Caudill <herb@devresults.com>
2019-10-20 14:26:24 +0200

    problems/043.ts: comments

--- src/problems/043.ts
+++ src/problems/043.ts
@@ -23,8 +23,11 @@ import { digits } from 'lib/digits'
 // Find the sum of all 0 to 9 pandigital numbers with this property.
 
 const divisors = [1, 2, 3, 5, 7, 11, 13, 17]
+
 const isSubstringDivisible = (n: number) =>
-  !divisors.some((d, i) => +n.toString().slice(i, i + 3) % d > 0)
+  !divisors
+    .slice(10 - n.toString().length)
+    .some((d, i) => +n.toString().slice(i, i + 3) % d > 0)
 
 expect(isSubstringDivisible(1406357289)).toBe(true)
 expect(isSubstringDivisible(1043652789)).toBe(false)
@@ -33,11 +36,29 @@ expect(isSubstringDivisible(1043652789)).toBe(false)
  * This works:
  *
  * ```ts
- *   export const solution043 = () => sum(panDigitals(10).filter(isSubstringDivisible))
+ *   const validPanDigitals = panDigitals(10).filter(isSubstringDivisible)
  * ```
  *
  * But it's slow (just generating all 10-pandigital numbers takes ~6s).
+ *
+ * A new strategy:
+ *
+ * - Generate all possibilities for the final 3-digit block: all 3-digit multiples of 17 with no
+ *   duplicated digits.
+ * - For each of these, generate all possibilities for the middle 3-digit block: same as above with
+ *   multiples of 7. Filter the combinations for sub-string divisibility.
+ * - For each of these, generate all possibilities for the block starting at the second digit:
+ *   multiples of 2. Filter all these combinations for sub-string divisibility.
+ * - For each of these, there will just be one possible digit for the
  */
+
+// so we also need to be able to test shorter strings at the tail end:
+expect(isSubstringDivisible(357289)).toBe(true) // xxxx357289
+expect(isSubstringDivisible(6357289)).toBe(true) // xxx6357289
+expect(isSubstringDivisible(406357289)).toBe(true) // x406357289
+expect(isSubstringDivisible(375829)).toBe(false)
+expect(isSubstringDivisible(6375298)).toBe(false)
+expect(isSubstringDivisible(460537892)).toBe(false)
 
 /**
  * Returns true
@@ -85,35 +106,41 @@ const candidates = (n: number): number[] => {
     .filter(noDuplicates)
 }
 
-expect(
-  candidates(17) //
-    .filter(noOverlap())
-    .slice(0, 5)
-).toEqual([102, 136, 153, 170, 187])
+const lastBlock = candidates(17)
 
-expect(
-  candidates(13)
-    .filter(noOverlap(102))
-    .slice(0, 5)
-).toEqual([364, 468, 546, 598, 637])
+// prettier-ignore
+expect(lastBlock).toEqual([102, 136, 153, 170, 187, 204, 238, 289, 306, 340, 357, 374, 391, 408, 425, 459, 476, 493, 510, 527, 561, 578, 612, 629, 680, 697, 714, 731, 748, 765, 782, 816, 850, 867, 901, 918, 935, 952, 986, ])
 
-export const solution043 = () =>
-  sum(
-    candidates(17)
-      .flatMap(d1 =>
-        candidates(7)
-          .filter(noOverlap(d1))
-          .map(d2 => +`${d2}${d1}`)
-      )
-      .flatMap(d2 =>
-        candidates(2)
-          .filter(noOverlap(d2))
-          .map(d3 => +`${d3}${d2}`)
-      )
-      .flatMap(d3 =>
-        allDigits //
-          .filter(noOverlap(d3))
-          .map(d4 => +`${d4}${d3}`)
-      )
-      .filter(isSubstringDivisible)
+const addMiddleBlock = lastBlock
+  .flatMap(d1 =>
+    candidates(7)
+      .filter(noOverlap(d1))
+      .map(d2 => +`${d2}${d1}`)
   )
+  .filter(isSubstringDivisible)
+
+// prettier-ignore
+expect(addMiddleBlock).toEqual([735204, 679238, 357289, 791306, 280391, 392476, 693510, 294680, 462731, 140782, 574816, 952867, 420918, 175986])
+
+const addFirstBlock = addMiddleBlock
+  .flatMap(d2 =>
+    candidates(2)
+      .filter(noOverlap(d2))
+      .map(d3 => +`${d3}${d2}`)
+  )
+  .filter(isSubstringDivisible)
+
+// prettier-ignore
+expect(addFirstBlock).toEqual([106357289, 160357289, 406357289, 460357289, 130952867, 430952867])
+
+const validPanDigitals = addFirstBlock //
+  .flatMap(d3 =>
+    allDigits //
+      .filter(noOverlap(d3))
+      .map(d4 => +`${d4}${d3}`)
+  )
+
+// prettier-ignore
+expect(validPanDigitals).toEqual([4106357289, 4160357289, 1406357289, 1460357289, 4130952867, 1430952867])
+
+export const solution043 = () => sum(validPanDigitals)
