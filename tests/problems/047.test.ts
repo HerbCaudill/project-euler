fbc24f7ef789604ff1d51a114013d5e9f0f8ffb9
Herb Caudill <herb@devresults.com>
2019-10-24 09:40:21 +0200

    restore missing test files

---
+++ tests/problems/047.test.ts
@@ -0,0 +1,3 @@
+import { solution047 } from '../../src/problems/047'
+import { executeTest } from '../../src/execute'
+executeTest(47, solution047)
---
+++ tests/problems/048.test.ts
@@ -0,0 +1,3 @@
+import { solution048 } from '../../src/problems/048'
+import { executeTest } from '../../src/execute'
+executeTest(48, solution048)
