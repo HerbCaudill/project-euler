10dfee39a5ff6e8e61069d3571b5ed12dd791f9f
Herb Caudill <herb@devresults.com>
2019-10-26 13:08:49 +0200

    problems/051.ts: still slow

--- src/problems/051.ts
+++ src/problems/051.ts
@@ -24,32 +24,81 @@ type Family = {
   length: number
 }
 
-const replaceDigits = (pattern: string) =>
-  allDigits.map(d => pattern.replace(/\*/g, d.toString())).map(Number)
+/**
+ * Replaces the digits marked as `*` with each digit from 0 to 9. Example:
+ * ```ts
+ * digitReplacements('*57') // 157, 257, 357, 457, 557, 657, 757, 857, 957
+ * ```
+ * @param pattern The numeric pattern to replace, consisting of digits and the `*` character.
+ */
+const digitReplacements = (pattern: string) =>
+  allDigits
+    .map(d => pattern.replace(/\*/g, d.toString()))
+    .map(Number)
+    .filter(n => n.toString().length === pattern.length) // don't allow leading zeroes
 
-// prettier-ignore
-expect(replaceDigits('*57')).toEqual([ 157, 257, 357, 457, 557, 657, 757, 857, 957, 57])
-// prettier-ignore
-expect(replaceDigits('1**')).toEqual([ 111, 122, 133, 144, 155, 166, 177, 188, 199, 100])
+{
+  // prettier-ignore
+  expect(digitReplacements('*57')).toEqual([ 157, 257, 357, 457, 557, 657, 757, 857, 957])
+  // prettier-ignore
+  expect(digitReplacements('1**')).toEqual([ 111, 122, 133, 144, 155, 166, 177, 188, 199, 100])
+}
 
+/**
+ * Same as `replaceDigits` but only returns prime values
+ * @param pattern
+ */
 const primeDigitReplacements = (pattern: string) =>
-  replaceDigits(pattern)
-    .filter(isPrime)
-    .sort()
+  digitReplacements(pattern).filter(isPrime)
 
-expect(primeDigitReplacements('*57')).toEqual([157, 257, 457, 557, 757, 857])
-// prettier-ignore
-expect(primeDigitReplacements('56**3')).toEqual([56003, 56113, 56333, 56443, 56663, 56773, 56993])
+{
+  expect(primeDigitReplacements('*57')).toEqual([157, 257, 457, 557, 757, 857])
+  // prettier-ignore
+  expect(primeDigitReplacements('56**3')).toEqual([56113, 56333, 56443, 56663, 56773, 56993, 56003])
+}
 
-const getPositionCombinations = memoize((length: number) => {
+/**
+ * Returns all possible combinations of character positions in a string of a given length (excluding
+ * the combination that consists of all character positions, because `****` isn't an interest
+ * pattern). For example, `getPositionCombinations(4)` would return the following:
+ * ```ts
+ *   0,1,2  0,1,3  0,2,3  1,2,3
+ *   0,1    0,2    0,3    1,2     1,3    2,3
+ *   0      1      2      3
+ * ```
+ */
+const _getPositionCombinations = (length: number) => {
   const positions = range(0, length - 1)
   const notEntireString = (positions: number[]) => positions.length < length
-  const result = combinations(positions).filter(notEntireString)
-  console.log('getPositionCombinations', length, result)
-  return result
-})
+  return combinations(positions).filter(notEntireString)
+}
+const getPositionCombinations = memoize(_getPositionCombinations)
 
-const allReplacementPatterns = (n: number) => {
+{
+  expect(getPositionCombinations(4)).toEqual([
+    [0, 1, 2],
+    [0, 1, 3],
+    [0, 2, 3],
+    [1, 2, 3],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 2],
+    [1, 3],
+    [2, 3],
+    [0],
+    [1],
+    [2],
+    [3],
+  ])
+}
+
+/**
+ * Finds all replacement patterns for a givern number. For example, for the number `157` you'd have:
+ * ` **7   *5*   1**   *57   1*7   15* `
+ * @param n
+ */
+const allPatterns = (n: number) => {
   const _n = n.toString().split('')
   const positionCombinations = getPositionCombinations(_n.length)
   const substituteStars = (pos: number[]) =>
@@ -59,26 +108,30 @@ const allReplacementPatterns = (n: number) => {
   return positionCombinations.map(substituteStars)
 }
 
-expect(allReplacementPatterns(157)).toEqual(
-  expect.arrayContaining(['**7', '*5*', '1**', '*57', '1*7', '15*'])
-)
+{
+  expect(allPatterns(157)).toEqual(
+    expect.arrayContaining(['**7', '*5*', '1**', '*57', '1*7', '15*'])
+  )
+}
 
 const findLongestFamilies = (digitCount: number) => {
   const candidates = range(10 ** (digitCount - 1), 10 ** digitCount - 1) // e.g. 3 digits -> 100..999
   const seen = new Set<string>()
-  const families: Family[] = candidates
-    .map(n =>
-      allReplacementPatterns(n)
-        .filter(p => !seen.has(p))
-        .map(pattern => {
-          // don't revisit this pattern
-          seen.add(pattern)
-          // generate families only containing primes
-          const family = primeDigitReplacements(pattern)
-          const length = family.length
-          return { pattern, length }
-        })
-    )
+
+  console.time(`patterns ${digitCount}`)
+  const patterns = candidates
+    .filter(isPrime)
+    .map(allPatterns)
+    .flat()
+  const distinctPatterns = Array.from(new Set<string>(patterns))
+  console.timeEnd(`patterns ${digitCount}`)
+  // generate prime digit replacement families & record their length
+  const generateFamilies = (pattern: string) => ({
+    pattern,
+    length: primeDigitReplacements(pattern).length,
+  })
+  const families: Family[] = distinctPatterns
+    .map(generateFamilies)
     .flat()
     .filter(f => f.length > 1)
   const findMax = (max: number, f: Family) => (f.length > max ? f.length : max)
@@ -86,11 +139,17 @@ const findLongestFamilies = (digitCount: number) => {
   return families.filter(f => f.length === maxLength)
 }
 
-for (const d of range(3, 6)) {
-  const label = `${d} digits`
-  console.time(label)
-  console.log(findLongestFamilies(d)[0].length)
-  console.timeEnd(label)
+{
+  const testLength = (digitCount: number, expectedLength: number) => {
+    const label = `${digitCount} digits`
+    console.time(label)
+    expect(findLongestFamilies(digitCount)[0].length).toEqual(expectedLength)
+    console.timeEnd(label)
+  }
+  testLength(3, 6)
+  testLength(4, 6)
+  testLength(5, 7)
+  testLength(6, 8)
 }
 
 const findSmallestFamilyMember = (families: Family[]) => {
@@ -103,7 +162,6 @@ const findSmallestFamilyMember = (families: Family[]) => {
 expect(findSmallestFamilyMember(findLongestFamilies(3))).toBe(107)
 
 export const solution051 = () => {
-  return -1
   let digitCount = 3
   let bestLength = 0
   let families: Family[] = []
@@ -111,7 +169,6 @@ export const solution051 = () => {
     families = findLongestFamilies(digitCount)
     const length = families[0].length
     if (length > bestLength) bestLength = length
-    console.log({ digitCount, bestLength })
     digitCount++
   } while (bestLength < 8 && digitCount < 7)
   return findSmallestFamilyMember(families)
