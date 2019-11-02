461ae54ff0ebf3f6e006bd207520f1058acc2494
Herb Caudill <herb@devresults.com>
2019-11-02 09:02:30 +0100

    problems/getFrequency.ts: create

---
+++ src/problems/getFrequency.ts
@@ -0,0 +1,10 @@
+﻿export const getFrequency = (b: Buffer) =>
+  sortByValue(b.reduce(frequencyReducer, {} as FrequencyTable))
+type FrequencyTable = {
+  [key: number]: number
+}
+const frequencyReducer = (r: any, d: number) => ({ ...r, [d]: (r[d] || 0) + 1 })
+const sortByValue = (obj: { [k: string]: number }) =>
+  Object.entries(obj)
+    .sort((b, a) => a[1] - b[1])
+    .map(e => +e[0])
