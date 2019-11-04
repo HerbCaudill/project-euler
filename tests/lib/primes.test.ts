8178afb186e0768ea89e0adbce328330d82f4f0b
Herb Caudill <herb@devresults.com>
2019-11-04 13:23:02 +0100

    tests/lib/primes.test.ts:

--- tests/lib/primes.test.ts
+++ tests/lib/primes.test.ts
@@ -1,10 +1,4 @@
-import {
-  isPrime,
-  nextPrime,
-  nthPrime,
-  primesUpTo,
-  probablyPrime,
-} from '../../src/lib/primes'
+import { isPrime, nextPrime, nthPrime, primesUpTo } from '../../src/lib/primes'
 
 describe('primesUpTo', () => {
   test('1', () => expect(primesUpTo(1)).toEqual([]))
@@ -53,30 +47,6 @@ describe('nthPrime', () => {
 describe('isPrime', () => {
   const testCase = (n: number, expected: boolean) =>
     expect(isPrime(n)).toEqual(expected)
-
-  test('0', () => testCase(0, false))
-  test('1', () => testCase(1, false))
-  test('2', () => testCase(2, true))
-  test('3', () => testCase(3, true))
-  test('4', () => testCase(4, false))
-  test('83', () => testCase(83, true))
-  test('7919', () => testCase(7919, true))
-  test('7920', () => testCase(7920, false))
-  test('7921', () => testCase(7921, false))
-  test('62710559', () => testCase(62710559, true))
-  test('62710561', () => testCase(62710561, false))
-  test('62710573', () => testCase(62710573, true))
-  test('71234567', () => testCase(71234567, true))
-  test('71234569', () => testCase(71234569, false))
-  test('10000000019', () => testCase(10000000019, true))
-  test('100000000000', () => testCase(100000000000, false))
-  test('100000000019', () => testCase(100000000019, true))
-  // test('9007199254740881', () => testCase(9007199254740881, true)) // largest prime in js integer space
-})
-
-describe('probablyPrime', () => {
-  const testCase = (n: number, expected: boolean) =>
-    expect(probablyPrime(n, 30)).toEqual(expected)
 
   test('0', () => testCase(0, false))
   test('1', () => testCase(1, false))
