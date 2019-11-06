d58d96b380f54d9519faa332c53bd0ad9b0a1036
Herb Caudill <herb@devresults.com>
2019-11-06 11:00:47 +0100

    problems/060.ts: down to 15 sec

--- src/problems/060.ts
+++ src/problems/060.ts
@@ -1,9 +1,8 @@
 import memoize from 'fast-memoize'
 import fs from 'fs'
 import { combinations } from 'lib/combinations'
-import { isPrime } from 'lib/miller-rabin'
-import { primesUpTo } from 'lib/primes'
-import { arrayAscending } from 'lib/sort'
+import { isPrime, primesUpTo } from 'lib/primes'
+import { ascending, arrayAscending } from 'lib/sort'
 import { sum } from 'lib/sum'
 const profiler = require('v8-profiler-node8')
 
@@ -24,67 +23,127 @@ const concat = (a: number, b: number) =>
 
 /** Tests whether a pair of numbers a & b concatenate both ways (a & b, b & a) to form prime
  * numbers. Doesn't test a or b for primality. */
-const _isPrimePair = ([a, b]: number[]) =>
-  isPrime(concat(a, b)) && isPrime(concat(b, a))
-const isPrimePair = memoize(_isPrimePair)
-const isntPrimePair = (p: number[]) => !isPrimePair(p)
+const primePairLookup = {} as { [key: number]: boolean }
+const getKey = ([a, b]: number[]) => ((a + b) * (a + b + 1)) / 2 + a // Cantor pairing function
+const isPrimePair = ([a, b]: number[]) => {
+  const key = getKey([a, b])
+  if (primePairLookup.hasOwnProperty(key)) return primePairLookup[key]
+  const result = (primePairLookup[key] =
+    isPrime(concat(a, b)) && isPrime(concat(b, a)))
+  return result
+}
 {
   expect(isPrimePair([3, 7])).toBe(true)
   expect(isPrimePair([7, 109])).toBe(true)
   expect(isPrimePair([3, 109])).toBe(true)
   expect(isPrimePair([109, 673])).toBe(true)
   expect(isPrimePair([113, 673])).toBe(false)
-
-  expect(isntPrimePair([113, 673])).toBe(true)
 }
 
-const isPrimePairSet = (arr: number[]) =>
-  !combinations(arr, 2).some(isntPrimePair)
+/**
+ * Our strategy is to find all sets of size 2, and then use those to find all sets of size 3, and so
+ * on until N (N=5 for this problem).
+ *
+ * For example, starting with primes up to 1000, we get sets of 2 like these:
+ * ```ts
+ *  [3,7],   [3,11],  [3,17],  [3,31],  [3,37],
+ *  [3,59],  [3,67],  [3,73],  [3,109], [3,137],
+ *  [3,191], [3,229], [3,271], [3,331], [3,359],
+ *  [3,373], [3,449], [3,467], [3,499], [3,541],
+ *  [3,557], [3,607], [3,613], [3,617], [3,673],
+ *  [3,701], [3,719], [3,733], [3,739], [3,823],
+ *  [3,929], [3,947], [7,19],  [7,61],  [7,97],
+ *  // ...
+ * ```
+ * We proceed by daisy-chaining these - e.g. `[3,7]` with `[7,97]` - by finding sets that partially
+ * overlap, to get sets of 3 like `[3,7,97]`. We then filter these sets to keep only those that are
+ * prime pair sets.
+ *
+ * ```ts
+ *  [3,7,109],    [3,7,229],    [3,7,541],    [3,7,673],
+ *  [3,7,823],    [3,11,701],   [3,17,449],   [3,37,67],
+ *  [3,37,607],   [3,59,929],   [3,73,607],   [3,73,823],
+ *  [3,109,673],  [3,137,191],  [3,137,359],  [3,137,947],
+ *  [3,229,373],  [3,229,499],  [3,229,613],  [3,331,739],
+ *  [3,359,701],  [3,373,823],  [3,449,557],  [3,467,617],
+ *  [3,499,673],  [3,613,673],  [3,719,947],  [7,19,97],
+ *  [7,19,433],   [7,19,937],   [7,61,487],   [7,97,829],
+ *  // ...
+ * ```
+ * One more pass gets us just two sets of 4:
+ * ```ts
+ *  [3,7,109,673],
+ *  23,311,677,827]
+ * ```
+ */
 
-function distinct(sets: number[][]) {
-  const stringify = (s: any) => s.sort().join(',')
-  const parse = (s: string) => s.split(',').map(d => +d)
-  return [...new Set(sets.map(stringify))].map(parse)
+function arraysEqual(a: number[], b: number[]) {
+  if (a === b) return true
+  if (a == null || b == null) return false
+  if (a.length != b.length) return false
+  for (var i = 0; i < a.length; ++i) if (a[i] !== b[i]) return false
+  return true
 }
 
-const mergeOverlappingSets1 = (sets: number[][]) => {
-  const result = []
+const extendSets = (sets: number[][]) => {
+  const extendedSets = []
+  // Group sets of N by their first N-1 items, and for each group make a list of the *final* item of
+  // each set. Then see if we can make prime pairs from those final items.
 
-  for (let i = 0; i < sets.length; i++) {
-    const set1 = sets[i]
-    for (let j = i + 1; j < sets.length; j++) {
-      const set2 = sets[j]
-      const additionalItems = getAdditionalItems(set2, set1)
-      if (additionalItems.length === 1)
-        result.push(set1.concat(additionalItems))
+  // For example with N = 2 we have a bunch of sets starting with 3:
+
+  // [3,7],   [3,11],  [3,17],  [3,31],  [3,37], ... [3,109], ... [3,947]
+
+  // So let's take the final item from each of these pairs and see if any of them make prime pairs.
+
+  // In this case, for example, [7, 109] is a prime pair; so we know that [3,7,109] is a prime pair
+  // set.
+
+  let initialValues = [] as number[]
+  let finalValues = [] as number[]
+  for (const set of sets) {
+    const newInitialValues = set.slice(0, set.length - 1)
+    const finalValue = set[set.length - 1]
+    if (arraysEqual(initialValues, newInitialValues)) {
+      // same initial items as previous
+      finalValues.push(finalValue)
+    } else {
+      // new set of initial items
+      if (finalValues.length) {
+        const newPrimePairs = combinations(finalValues, 2).filter(isPrimePair)
+        for (const pair of newPrimePairs)
+          extendedSets.push(initialValues.concat(pair).sort(ascending))
+      }
+      initialValues = newInitialValues
+      finalValues = [finalValue]
     }
   }
-  return distinct(result)
-    .filter(isPrimePairSet)
-    .sort(arrayAscending)
+
+  const result = extendedSets.sort(arrayAscending)
+  return result
 }
 
 const findSets = (max: number, size: number) => {
-  let sets = primesUpTo(max).map(p => [p]) // start out with 'sets' of one
-  let i = 1
-  while (i++ < size) sets = mergeOverlappingSets1(sets)
+  let primePairs = new Set(combinations(primesUpTo(max), 2).filter(isPrimePair))
+  let sets = [...primePairs] // start with sets of 2
+  let i = 2
+  while (i++ < size) sets = extendSets(sets)
   return sets
 }
 
-function getAdditionalItems(set2: number[], set1: number[]) {
-  return set2.filter(d => !set1.includes(d))
+export const solution060 = () => {
+  let s = [] as number[][]
+  profile(() => (s = findSets(10000, 5)))
+  return sum(s[0])
 }
 
-export const solution060 = () => {
-  profiler.startProfiling('1')
-  console.time(`findSets`)
-  const s = findSets(10000, 5)
-  console.timeEnd(`findSets`)
+const profile = (fn: Function) => {
+  profiler.startProfiling()
+  fn()
   const profile = profiler.stopProfiling()
 
   profile.export((err: any, result: any) => {
     fs.writeFileSync('profile.cpuprofile', result)
     profile.delete()
   })
-  return sum(s[0])
 }
