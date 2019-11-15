15811017360421490d0126ac876e694ada6c81a5
Herb Caudill <herb@devresults.com>
2019-11-15 13:32:15 +0100

    tests/lib/polygonalNumbers.test.ts:

--- src/tests/lib/polygonalNumbers.test.ts
+++ src/tests/lib/polygonalNumbers.test.ts
@@ -1,4 +1,4 @@
-import { isPolygonal } from '../../lib/polygonalNumbers'
+import { isPolygonal, PolygonalSequence } from '../../lib/polygonalNumbers'
 
 describe('polygonal numbers', () => {
   test.each`
@@ -39,4 +39,15 @@ describe('polygonal numbers', () => {
   `(`pentagonal numbers`, ({ n, expected }) => {
     expect(isPolygonal(5)(n)).toBe(expected)
   })
+})
+
+describe('PolygonalSequence', () => {
+  it('triangular', () =>
+    expect(new PolygonalSequence(3).valuesUpTo(20)).toEqual([1, 3, 6, 10, 15]))
+  it('square', () =>
+    expect(new PolygonalSequence(4).valuesUpTo(30)).toEqual([1, 4, 9, 16, 25]))
+  it('pentagonal', () =>
+    expect(new PolygonalSequence(5).valuesUpTo(40)).toEqual([1, 5, 12, 22, 35]))
+  it('hexagonal', () =>
+    expect(new PolygonalSequence(6).valuesUpTo(50)).toEqual([1, 6, 15, 28, 45]))
 })
