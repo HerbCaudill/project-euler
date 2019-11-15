9584ffcda4992b6c25d0f4d44e962d6acf456913
Herb Caudill <herb@devresults.com>
2019-11-15 13:31:56 +0100

    lib/commonValue.ts: create

---
+++ src/lib/commonValue.ts
@@ -0,0 +1,10 @@
+﻿import { numberLength } from './numberLength'
+
+export const commonValue = (set: number[], fn: (d: number) => number) => {
+  const values = set.map(fn)
+  const result = values[0]
+  if (values.some(v => result !== v)) return undefined
+  return result
+}
+
+export const commonLength = (set: number[]) => commonValue(set, numberLength)
