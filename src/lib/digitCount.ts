3431d7e9ba7b3776f345977aa762aa00ccd69dc5
Herb Caudill <herb@devresults.com>
2019-11-17 13:24:32 +0100

    lib/digitCount.ts:

--- src/lib/digitCount.ts
+++ src/lib/digitCount.ts
@@ -1,2 +1,4 @@
-﻿export const digitCount = (n: number) => Math.trunc(Math.log10(n)) - 1
+﻿export const digitCount = (n: number) =>
+  n === 0 ? 1 : Math.trunc(Math.log10(n)) + 1
+
 export const hasXDigits = (x: number) => (n: number) => digitCount(n) === x
