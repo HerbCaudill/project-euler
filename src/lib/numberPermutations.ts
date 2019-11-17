c49120390fdcc56b1a28f941fb55f6837532c142
Herb Caudill <herb@devresults.com>
2019-11-17 13:24:36 +0100

    lib/numberPermutations.ts: create

---
+++ src/lib/numberPermutations.ts
@@ -0,0 +1,14 @@
+﻿import { permutations } from 'lib/permutations'
+import { digits } from 'lib/digits'
+import { hasXDigits, digitCount } from 'lib/digitCount'
+import { distinct } from 'lib/distinct'
+import { ascending } from 'lib/sort'
+
+export const numberPermutations = (n: number) =>
+  distinct(
+    permutations(digits(n))
+      .map(p => p.join(''))
+      .map(Number)
+  )
+    .filter(hasXDigits(digitCount(n)))
+    .sort(ascending)
