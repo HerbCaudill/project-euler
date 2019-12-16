bd90b8c9b458d21233f0c5674aeb60ff88d25c5b
Herb Caudill <herb@devresults.com>
2019-12-16 12:23:04 +0100

    lib/distinct.ts: faster; distinctSorted

--- src/lib/distinct.ts
+++ src/lib/distinct.ts
@@ -1,1 +1,58 @@
-﻿export const distinct = (arr: number[]) => [...new Set(arr)]
+﻿// export const distinct = (arr: number[]) => [...new Set(arr)]
+
+const LARGE_ARRAY_SIZE = 200
+
+export const distinct = (array: number[]) => {
+  let index = -1
+  let isCommon = true
+
+  const { length } = array
+  const result: number[] = []
+  let seen = result
+
+  if (length >= LARGE_ARRAY_SIZE) {
+    return [...new Set(array)]
+  } else {
+    seen = result
+  }
+  outer: while (++index < length) {
+    let value = array[index]
+    const computed = value
+
+    value = value !== 0 ? value : 0
+    if (isCommon && computed === computed) {
+      let seenIndex = seen.length
+      while (seenIndex--) {
+        if (seen[seenIndex] === computed) {
+          continue outer
+        }
+      }
+      result.push(value)
+    } else if (!seen.some(array.includes)) {
+      if (seen !== result) {
+        seen.push(computed)
+      }
+      result.push(value)
+    }
+  }
+  return result
+}
+
+export const distinctSorted = (array: number[]) => {
+  let seen
+  let index = -1
+  let resIndex = 0
+
+  const { length } = array
+  const result = []
+
+  while (++index < length) {
+    const value = array[index],
+      computed = value
+    if (!index || computed !== seen) {
+      seen = computed
+      result[resIndex++] = value === 0 ? 0 : value
+    }
+  }
+  return result
+}
